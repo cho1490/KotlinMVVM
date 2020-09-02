18913031c3b310361f61286d2a1f9cba89d0de5e
cho1490 <chosanghyun99@gmail.com>
2020-09-03 02:23:31 +0900

    Start MVVM

--- app/src/main/java/com/example/mytodo/model/TodoModel.kt
+++ app/src/main/java/com/example/mytodo/model/TodoModel.kt
@@ -1,7 +1,21 @@
 package com.example.mytodo.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Todo")
 data class TodoModel (
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Long?,
+
+    @ColumnInfo(name = "title")
     var title: String,
+
+    //https://doitddo.tistory.com/54?category=855312
     var description: String,
+
+
     var createdDate: Long
 ){}
