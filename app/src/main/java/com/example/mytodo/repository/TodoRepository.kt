52d1c1901d9d9fda85490100f6bb62c9cb67f5a3
cho1490 <chosanghyun99@gmail.com>
2020-09-03 23:48:39 +0900

    Add RxKotlin

--- app/src/main/java/com/example/mytodo/repository/TodoRepository.kt
+++ app/src/main/java/com/example/mytodo/repository/TodoRepository.kt
@@ -1,10 +1,17 @@
 package com.example.mytodo.repository
 
+
 import android.app.Application
 import androidx.lifecycle.LiveData
 import com.example.mytodo.database.TodoDAO
 import com.example.mytodo.database.TodoDatabase
 import com.example.mytodo.model.TodoModel
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.schedulers.Schedulers
 
 class TodoRepository(application : Application){
 
@@ -22,10 +29,21 @@ class TodoRepository(application : Application){
         return mTodoItems
     }
 
+
     fun insertTodo(todoModel : TodoModel){
-        Thread(Runnable {
-            mTodoDAO.insertTodo(todoModel)
-        }).start()
+        val compositeDisposable : CompositeDisposable? = null
+        val disposable : Disposable = Single.just(todoModel)
+            .subscribeOn(Schedulers.io())
+            .subscribe({
+                mTodoDAO.insertTodo(todoModel)
+            }, {
+                //error
+            })
+
+        compositeDisposable!!.add(disposable)
+        disposable.addTo(compositeDisposable)
+
+        compositeDisposable.dispose();
     }
 
 }
